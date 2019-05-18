a7ad4c82b27770d6dcad90fb3c602f643efda2f1
hideaki matsunami <carbon0409@gmail.com>
2019-05-18 15:12:40 +0900

    Nestedにした

--- src/test/java/mahaker/tdd/money/MoneyTest.java
+++ src/test/java/mahaker/tdd/money/MoneyTest.java
@@ -3,20 +3,27 @@ package mahaker.tdd.money;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("moneyパッケージ内のクラスのテスト")
 public class MoneyTest {
 
-  @DisplayName("$5の2倍は$10である")
-  @Test
-  public void testMultiplication() {
-    // arrange
-    final Dollar five = new Dollar(5);
+  @DisplayName("Dollarクラスのテスト")
+  @Nested
+  public class DollarTest {
 
-    // action
-    five.times(2);
+    @DisplayName("$5の2倍は$10である")
+    @Test
+    public void testMultiplication() {
+      // arrange
+      final Dollar five = new Dollar(5);
 
-    // assert
-    assertEquals(10, five.amount);
+      // action
+      five.times(2);
+
+      // assert
+      assertEquals(10, five.amount);
+    }
   }
 }
